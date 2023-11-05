e21ad042b7f1b7daa1333272eabbaa8c5a5d810e
Von Harris <vonharris00@gmail.com>
2023-11-05 18:36:04 -0500

    Completed Project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    const email = document.querySelector("#email");
+    const message = document.querySelector(".message");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const userEmail = email.value.trim();
+
+        if (userEmail === "") {
+            message.textContent = "Please enter a valid email address.";
+        } else {
+            message.textContent = `Thank you! Your email address ${userEmail} has been added to our mailing list!`;
+        }
+
+    });
+});
